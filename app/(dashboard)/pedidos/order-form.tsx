626868e0facb7315904c407778dc3f5b4545d7ba
Sebastian Blanco <sebastianblancogonz@gmail.com>
2025-04-20 01:29:28 +0200

    feat: add possibility to remove images

--- app/(dashboard)/pedidos/order-form.tsx
+++ app/(dashboard)/pedidos/order-form.tsx
@@ -3,9 +3,20 @@ import { Button } from '@/components/ui/button';
 import { Order, OrderHistory } from '@types';
 import { PaymentMethod, OrderStatus, ProductType, PaymentStatus } from '@types';
 
-const CLOUDINARY_UPLOAD_URL =
-  'https://api.cloudinary.com/v1_1/TU_CLOUD_NAME/image/upload';
-const CLOUDINARY_UPLOAD_PRESET = 'TU_UPLOAD_PRESET';
+const productTypeOptions = [
+  { value: ProductType.Tarta, label: 'Tarta' },
+  { value: ProductType.Galletas, label: 'Galletas' },
+  { value: ProductType.Cupcakes, label: 'Cupcakes' },
+  { value: ProductType.Macarons, label: 'Macarons' },
+  { value: ProductType.Otros, label: 'Otros' }
+];
+
+const paymentMethodOptions = [
+  { value: PaymentMethod.Efectivo, label: 'Efectivo' },
+  { value: PaymentMethod.Tarjeta, label: 'Tarjeta' },
+  { value: PaymentMethod.Transferencia, label: 'Transferencia' },
+  { value: PaymentMethod.Bizum, label: 'Bizum' }
+];
 
 const OrderForm = ({
   setIsModalOpen,
@@ -20,18 +31,15 @@ const OrderForm = ({
   setIsCreating: (value: boolean) => void;
   orderToEdit: Order | null;
 }) => {
+  const [imageUrls, setImageUrls] = useState<string[]>(
+    orderToEdit?.images || []
+  );
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     const formData = new FormData(e.currentTarget);
-
-    if (orderToEdit) {
-      const order: Order = buildOrder(formData, orderToEdit);
-      updateOrder(order);
-    } else {
-      const order: Order = buildOrder(formData, null);
-      saveOrder(order);
-    }
+    const order = buildOrder(formData, orderToEdit);
+    orderToEdit ? updateOrder(order) : saveOrder(order);
   };
 
   const getFormValue = <T,>(
@@ -51,48 +59,75 @@ const OrderForm = ({
       let hasChanges = false;
 
       const fieldTransformers: Record<
-        keyof Order,
+        Exclude<
+          keyof Order,
+          | 'images'
+          | 'orderHistory'
+          | 'id'
+          | 'orderDate'
+          | 'orderStatus'
+          | 'paymentStatus'
+          | 'customizationDetails'
+        >,
         (value: FormDataEntryValue) => any
       > = {
-        id: (value) => value, // No se actualiza
-        orderDate: (value) => new Date(value.toString()),
-        orderStatus: (value) => value as OrderStatus,
-        paymentStatus: (value) => value as PaymentStatus,
-        customizationDetails: (value) => value.toString(),
-        orderHistory: (value) => JSON.parse(value.toString()) as OrderHistory[],
-        customerName: (value) => value.toString(),
-        customerContact: (value) => value.toString(),
-        description: (value) => value.toString(),
-        deliveryDate: (value) => new Date(value.toString()),
-        productType: (value) => value as ProductType,
-        quantity: (value) => parseInt(value.toString(), 10),
-        sizeOrWeight: (value) => value.toString(),
-        flavor: (value) => value.toString(),
-        allergyInformation: (value) => value.toString(),
-        paymentMethod: (value) => value as PaymentMethod,
-        notes: (value) => value.toString(),
-        amount: (value) => parseFloat(value.toString()),
-        totalPrice: (value) => parseFloat(value.toString()),
-        images: (value) => []
+        customerName: (v) => v.toString(),
+        customerContact: (v) => v.toString(),
+        description: (v) => v.toString(),
+        deliveryDate: (v) => new Date(v.toString()),
+        productType: (v) => v as ProductType,
+        quantity: (v) => parseInt(v.toString(), 10),
+        sizeOrWeight: (v) => v.toString(),
+        flavor: (v) => v.toString(),
+        allergyInformation: (v) => v.toString(),
+        paymentMethod: (v) => v as PaymentMethod,
+        notes: (v) => v.toString(),
+        amount: (v) => parseFloat(v.toString()),
+        totalPrice: (v) => parseFloat(v.toString())
       };
 
       for (const field in fieldTransformers) {
-        const key = field as keyof Order;
+        const key = field as keyof typeof fieldTransformers;
         const formValue = formData.get(field);
 
         if (formValue !== null) {
           const transformedValue = fieldTransformers[key](formValue);
+          const originalValue = existingOrder[key];
 
-          if (transformedValue !== existingOrder[key]) {
+          let valueChanged = false;
+          if (
+            originalValue instanceof Date &&
+            transformedValue instanceof Date
+          ) {
+            valueChanged =
+              originalValue.getTime() !== transformedValue.getTime();
+          } else {
+            valueChanged = transformedValue !== originalValue;
+          }
+
+          if (valueChanged) {
             updatedOrder[key] = transformedValue;
             hasChanges = true;
           }
         }
       }
 
+      const originalImages = existingOrder.images || [];
+      if (
+        imageUrls.length !== originalImages.length ||
+        JSON.stringify(imageUrls) !== JSON.stringify(originalImages)
+      ) {
+        updatedOrder.images = imageUrls;
+        hasChanges = true;
+        console.log('Image changes detected. New image array:', imageUrls);
+      } else {
+        console.log('No image changes detected.');
+      }
+
       return hasChanges ? { ...existingOrder, ...updatedOrder } : existingOrder;
     }
 
+    const newAmount = parseFloat(getFormValue(formData, 'amount', '0'));
     return {
       id: undefined,
       orderDate: new Date(),
@@ -103,9 +138,11 @@ const OrderForm = ({
       customerName: getFormValue(formData, 'customerName', ''),
       customerContact: getFormValue(formData, 'customerContact', ''),
       description: getFormValue(formData, 'description', ''),
-      deliveryDate: new Date(getFormValue(formData, 'deliveryDate', '')),
+      deliveryDate: new Date(
+        getFormValue(formData, 'deliveryDate', new Date().toISOString())
+      ),
       productType: getFormValue(formData, 'productType', ProductType.Otros),
-      quantity: parseInt(getFormValue(formData, 'quantity', '0')),
+      quantity: parseInt(getFormValue(formData, 'quantity', '1'), 10),
       sizeOrWeight: getFormValue(formData, 'sizeOrWeight', ''),
       flavor: getFormValue(formData, 'flavor', ''),
       allergyInformation: getFormValue(formData, 'allergyInformation', ''),
@@ -115,44 +152,104 @@ const OrderForm = ({
         PaymentMethod.Efectivo
       ),
       notes: getFormValue(formData, 'notes', ''),
-      amount: parseFloat(getFormValue(formData, 'amount', '0')),
-      totalPrice: parseFloat(getFormValue(formData, 'amount', '0')),
-      images: []
+      amount: newAmount,
+      totalPrice: newAmount,
+      images: imageUrls
     };
   };
 
-  async function saveOrder(order: Order) {
-    const response = await fetch('/api/orders', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(order)
-    });
-
-    if (response.ok) {
-      setOrders((orders) => [...orders, order]);
-      setIsModalOpen(false);
-    }
-
+  const closeModal = () => {
+    setIsModalOpen(false);
     setIsEditing(false);
     setIsCreating(false);
-  }
+  };
 
-  async function updateOrder(order: Order) {
-    const response = await fetch(`/api/orders/${Number(order.id)}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
+  const saveOrder = async (order: Order) => {
+    const res = await fetch('/api/orders', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(order)
     });
-
-    if (response.ok) {
-      setOrders((orders) => orders.map((o) => (o.id === order.id ? order : o)));
-      setIsModalOpen(false);
+    if (res.ok) {
+      setOrders((prev) => [...prev, order]);
+      closeModal();
     }
-  }
+  };
+
+  const updateOrder = async (order: Order) => {
+    const res = await fetch(`/api/orders/${order.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(order)
+    });
+    if (res.ok) {
+      setOrders((prev) => prev.map((o) => (o.id === order.id ? order : o)));
+      closeModal();
+    }
+  };
+
+  const fields = [
+    {
+      label: 'Cliente',
+      name: 'customerName',
+      type: 'text',
+      placeholder: 'Nombre del cliente'
+    },
+    {
+      label: 'Contacto del cliente',
+      name: 'customerContact',
+      type: 'text',
+      placeholder: 'Contacto del cliente'
+    },
+    {
+      label: 'Descripción',
+      name: 'description',
+      type: 'textarea',
+      placeholder: 'Descripción del encargo'
+    },
+    {
+      label: 'Fecha de entrega',
+      name: 'deliveryDate',
+      type: 'date',
+      placeholder: 'Fecha de entrega del encargo'
+    },
+    {
+      label: 'Cantidad',
+      name: 'quantity',
+      type: 'number',
+      placeholder: 'Cantidad de productos'
+    },
+    {
+      label: 'Tamaño o peso',
+      name: 'sizeOrWeight',
+      type: 'text',
+      placeholder: 'Tamaño o peso del encargo'
+    },
+    {
+      label: 'Sabor',
+      name: 'flavor',
+      type: 'text',
+      placeholder: 'Sabor'
+    },
+    {
+      label: 'Información alergias',
+      name: 'allergyInformation',
+      type: 'textarea',
+      placeholder: 'Alergias del cliente'
+    },
+    {
+      label: 'Notas',
+      name: 'notes',
+      type: 'textarea',
+      placeholder: 'Notas'
+    },
+    {
+      label: 'Importe',
+      name: 'amount',
+      type: 'number',
+      placeholder: 'Importe a cobrar'
+    }
+  ];
 
   return (
     <>
@@ -160,108 +257,77 @@ const OrderForm = ({
         {orderToEdit ? 'Editar Pedido' : 'Nuevo Pedido'}
       </h2>
       <form className="space-y-4" onSubmit={handleSubmit}>
-        <InputField
-          label="Cliente"
-          name="customerName"
-          placeholder="Nombre del cliente"
-          defaultValue={orderToEdit?.customerName}
-        />
-        <InputField
-          label="Contacto del cliente"
-          name="customerContact"
-          placeholder="Teléfono o email"
-          defaultValue={orderToEdit?.customerContact}
-        />
-        <TextAreaField
-          label="Descripción"
-          name="description"
-          placeholder="Descripción del pedido"
-          defaultValue={orderToEdit?.description}
-        />
-        <InputField
-          label="Fecha de entrega"
-          name="deliveryDate"
-          type="date"
-          defaultValue={
-            orderToEdit
-              ? orderToEdit?.deliveryDate instanceof Date
-                ? orderToEdit.deliveryDate.toISOString().split('T')[0]
-                : new Date(orderToEdit?.deliveryDate || '')
-                    .toISOString()
-                    .split('T')[0]
-              : new Date().toISOString()
-          }
-        />
+        {fields.map((field) =>
+          field.type === 'textarea' ? (
+            <TextAreaField
+              key={field.name}
+              label={field.label}
+              placeholder={field.placeholder}
+              name={field.name}
+              defaultValue={orderToEdit?.[field.name as keyof Order] as string}
+            />
+          ) : (
+            <InputField
+              key={field.name}
+              label={field.label}
+              name={field.name}
+              type={field.type}
+              placeholder={field.placeholder}
+              defaultValue={orderToEdit?.[
+                field.name as keyof Order
+              ]?.toString()}
+            />
+          )
+        )}
 
         <SelectField
           label="Tipo de producto"
           name="productType"
-          options={[
-            { value: ProductType.Tarta, label: 'Tarta' },
-            { value: ProductType.Galletas, label: 'Galletas' },
-            { value: ProductType.Cupcakes, label: 'Cupcakes' },
-            { value: ProductType.Macarons, label: 'Macarons' },
-            { value: ProductType.Otros, label: 'Otros' }
-          ]}
+          options={productTypeOptions}
           defaultValue={orderToEdit?.productType}
         />
-        <InputField
-          label="Cantidad"
-          name="quantity"
-          type="number"
-          defaultValue={orderToEdit?.quantity?.toString()}
-        />
-        <InputField
-          label="Tamaño o peso"
-          name="sizeOrWeight"
-          placeholder="Tamaño o peso del producto"
-          defaultValue={orderToEdit?.sizeOrWeight}
-        />
-        <InputField
-          label="Sabor"
-          name="flavor"
-          placeholder="Sabor del producto"
-          defaultValue={orderToEdit?.flavor}
-        />
-        <TextAreaField
-          label="Información alergias"
-          name="allergyInformation"
-          placeholder="Información sobre alergias"
-          defaultValue={orderToEdit?.allergyInformation}
-        />
+
         <SelectField
           label="Método de pago"
           name="paymentMethod"
-          options={[
-            { value: PaymentMethod.Efectivo, label: 'Efectivo' },
-            { value: PaymentMethod.Tarjeta, label: 'Tarjeta' },
-            {
-              value: PaymentMethod['Transferencia'],
-              label: 'Transferencia'
-            },
-            { value: PaymentMethod.Bizum, label: 'Bizum' }
-          ]}
+          options={paymentMethodOptions}
           defaultValue={orderToEdit?.paymentMethod}
         />
-        <TextAreaField
-          label="Notas"
-          name="notes"
-          placeholder="Notas adicionales"
-          defaultValue={orderToEdit?.notes}
-        />
-        <InputField
-          label="Importe"
-          name="amount"
-          type="number"
-          placeholder="0.00"
-          defaultValue={orderToEdit?.amount?.toString()}
-        />
+
+        {orderToEdit && imageUrls.length > 0 && (
+          <div className="space-y-2">
+            <label className="block text-sm font-medium text-gray-700">
+              Imágenes del pedido
+            </label>
+            <div className="flex flex-wrap gap-2">
+              {imageUrls.map((url, index) => (
+                <div key={index} className="relative w-24 h-24">
+                  <img
+                    src={url}
+                    alt={`Imagen ${index + 1}`}
+                    className="w-full h-full object-cover rounded-md"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => {
+                      console.log(imageUrls);
+                      setImageUrls((prev) => {
+                        return prev.filter((_, i) => i !== index);
+                      });
+                      console.log(imageUrls);
+                    }}
+                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1 text-xs hover:bg-red-600"
+                  >
+                    ×
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
         <div className="flex justify-end">
-          <Button
-            type="button"
-            variant="outline"
-            onClick={() => setIsModalOpen(false)}
-          >
+          <Button type="button" variant="outline" onClick={closeModal}>
             Cancelar
           </Button>
           <Button type="submit" className="ml-2">
