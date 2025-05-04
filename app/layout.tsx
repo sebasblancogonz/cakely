fe26a8fecfd52bc887a3147df4aeb44873b319d5
Sebastian Blanco <sebastianblancogonz@gmail.com>
2025-05-04 21:27:57 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,45 @@ import { SessionProvider } from 'next-auth/react';
 import { Loader2 } from 'lucide-react';
 
 export const metadata = {
-  title: 'Cakely',
+  title: 'Cakely – CRM para Repostería',
   description:
-    'Un panel de control para administrar pedidos, clientes y productos de tu negocio.'
+    'Cakely es un software de gestión de pedidos y clientes para pastelerías modernas. Organiza tu negocio de repostería de forma fácil y eficiente.',
+  keywords: [
+    'CRM para repostería',
+    'gestión de pedidos',
+    'software pastelería',
+    'CRM panaderías',
+    'cakely',
+    'gestión clientes repostería',
+    'CRM para negocios de tartas y galletas'
+  ],
+  authors: [{ name: 'Cakely', url: 'https://cakely.es' }],
+  creator: 'Cakely',
+  metadataBase: new URL('https://cakely.es'),
+  openGraph: {
+    title: 'Cakely – CRM para Repostería',
+    description:
+      'Gestiona clientes y pedidos de tu pastelería con facilidad usando Cakely.',
+    url: 'https://cakely.es',
+    siteName: 'Cakely',
+    images: [
+      {
+        url: '/logo.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Cakely – CRM para repostería'
+      }
+    ],
+    locale: 'es_ES',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cakely – CRM para Repostería',
+    description: 'Simplifica la gestión de tu pastelería con Cakely.',
+    images: ['/logo.webp'],
+    creator: '@cakelyapp'
+  }
 };
 
 export default function RootLayout({
